e43527c6ebd70d25e38b6a64be303f1b2a89bc45
myada siddig <siddmomo335@gmail.com>
2026-02-04 14:17:25 +0300

    Delete Exercises 2/country and nme.py

--- Exercises 2/country and nme.py
+++
@@ -1,8 +0,0 @@
-#step 1: create a dictionary to store personal information 
-bio = {
-    "name": "mayada",
-    "hometown": "riyadh",
-    "age": 23
-}
-#step 2: print each value on a separate line using one print() statement
-print(bio["name"],  "\n" + bio["hometown"],  "\n" + str(bio["age"]))
